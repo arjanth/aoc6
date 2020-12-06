3b4b5b486a7ba36ee56874ef88e3d46aba9be44d
Arjan ten Hoeve <arjan.ten.hoeve@achmea.nl>
2020-12-06 23:14:06 +0100

    het voorbeeld werkt

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 
 namespace aoc6
@@ -12,25 +13,50 @@ namespace aoc6
             var group = new List<char>();
             char[] row;
             int totalCount=0;
+            bool newgroup=true;
+            var removequestions = new List<char>();
             foreach (string line in File.ReadLines(@"C:\Repos\aoc6\input\example.txt"))
             {
                 Console.WriteLine(line);
                 if (line == string.Empty)  // group is ingelezen let op je moet twee lege enters na laatste groep in inputfile hebben
                 {
                     totalCount += group.Count;
-                    Console.WriteLine($"unieke antwoorden: {string.Join(",", group)} aantal: {group.Count}");
+                    Console.WriteLine($" antwoorden die iedere persoon uit de groep heeft gegeven: {string.Join(",", group)} aantal: {group.Count}");
                     group.Clear();  //wis de antwoorden voor een nieuwe group
+                    newgroup = true;
                 }
                 else
                 {
-                    row = line.ToCharArray();  //de antwoorden opslaan in een tijdelijke array
-                    foreach (char question in row)
+                    row = line.ToCharArray();  //de antwoorden opslaan in een tijdelijke array     
+                    if(newgroup)  //eerste persoon uit de group
                     {
-                        if (!group.Contains(question)) group.Add(question);   //sla de gelezen velden op in de password dictionary
+                        foreach (char question in row)
+                        {
+                           group.Add(question);   // alle antwoorden van het eerste groupslid opnemen in de lijst
+                        }
+                        newgroup = false;
                     }
+                    else // tweede of volgende persoon in de group
+                    {
+                        
+                        //antwoorden uit de grouplijst verwijderen die niet op de lijst van de huidige persoon staan
+                        foreach (char question in group)
+                        {
+                            if (!row.Contains(question)) removequestions.Add(question);      //maak een lijst van te verwijderen vragen
+                        }
+                        
+                        foreach(char question in removequestions)
+                        {
+                            group.Remove(question);  //verwijder de vraag van de grouplijst
+                        }
+                        removequestions.Clear();   //maak het tijdelijke lijstje weer leeg.
+                        
+                    }
+                    
                 }
             }
             Console.WriteLine($"sum of counts: {totalCount}");
+            // goede antwoord: 6457 (deel 1, anyone yes)
         }
     }
 }
